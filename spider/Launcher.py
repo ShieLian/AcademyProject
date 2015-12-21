af3b4f8a0352ed5e81388e2c42f5c83dad5bd6fc
ShieLian <947514447@qq.com>
2015-12-21 17:26:25 +0800

    some fixes of frame.py
    Also need to solve callbacks and birdges

--- spider/Launcher.py
+++ spider/Launcher.py
@@ -1,9 +1,16 @@
+#coding=UTF-8
 '''
 Created on 2015.11.30
 
 @author: ShieLian
 '''
-
+import spider.core.events as events
+from spider.gui.frame import *
+eventbus=events.EventBus()
+frame=Frame()
+'''
+@todo 监听注册&新建线程
+'''
 def main():
     pass
 
